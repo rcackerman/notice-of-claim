8211267c5d9d3f4589bf97bb8a2bb1d27dffafde
Rebecca <rebecca@casecommons.org>
2015-10-30 21:42:51 -0400

    Starting notice of claim app

---
+++ notice-of-claim.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+require 'haml'
+
+# multipart form
+# index -> demographic -> 
+get '/' do
+  haml :index
+end
+
+post '/' do
+  puts params
+  @lawyer = params['lawyer']
+  redirect to('/demographic/')
+end
+
+get '/demographic/' do
+  haml :demographic
+end
+
+post '/demographic/' do
+  puts params
+  redirect to('/incident/')
+end
+
+get '/incident/' do
+  haml :incident
+end
+
+post '/incident/' do
+  puts params
+  redirect to('/incident-details/')
+end
+
+get '/incident-details/' do
+  haml :incident_details
+end
+
+post '/incident-details/' do
+  puts params
+  redirect to('/review/')
+end
+
+get '/review/' do
+  haml :review
+end
+
+post '/review/' do
+  puts params
+end
