de8abe51c042ba2fa0fb2a9d0f024ee5dc4f599c
Rebecca <rcackerman@gmail.com>
2015-11-15 21:21:33 -0500

    Adding javascript

---
+++ public/js/notice-of-claim.js
@@ -0,0 +1,57 @@
+
+// - form
+// - - theme (intro, demographics, etc)
+// - - - fieldset
+
+var NOCForm = React.createClass({
+  render: function() {
+    //var themes = _.uniq(
+        //_.map(QUESTIONS,
+              //function(i) { return i.theme }
+        //)
+    //);
+    return (
+        Hello
+        //<NOCThemeGroup questions={this.props.questions}/>    
+    );
+    //return (
+      //themes.forEach(function(theme) {
+        //<div className={theme}>
+          //<NOCThemeGroup theme={theme} />
+        //</div>
+      //})
+    //);
+  }
+});
+
+//var NOCThemeGroup = React.createClass({
+  //render: function() {
+    //var themeQuestions = _.where(this.props.questions,
+                                  //{theme: "intro"}
+                                //);
+    //return (
+        //<p>Themes are: {this.props.questions.theme}</p>
+        //<div className="themes">
+          //<NOCFieldset questions={themeQuestions}/>
+        //</div>
+    //);
+  //}
+//});
+
+//var NOCFieldset = React.createClass({
+  //render: function() {
+    //<div className="checkbox">
+      //Fieldset
+    //</div>
+  //}
+//});
+
+//var QUESTIONS = [
+  //{theme: "intro"},
+  //{theme: "demographics"}
+//];
+
+ReactDOM.render(
+  <NOCForm />,
+  document.getElementById('main')
+);
