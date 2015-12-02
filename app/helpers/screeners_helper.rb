a2a9a2fe1c216b5a725a7556a8c66d8ba7c7817c
Rebecca <rcackerman@gmail.com>
2015-12-01 22:45:34 -0500

    Making the screener look better and giving users feedback on screened out incidents

--- app/helpers/screeners_helper.rb
+++ app/helpers/screeners_helper.rb
@@ -1,2 +1,11 @@
 module ScreenersHelper
+  def screen_claim(harm, incident_date)
+    incident_date = incident_date.to_date
+    if harm && (incident_date >= (Date.today - 90))
+      binding.pry
+      return true
+    else
+      return false
+    end
+  end
 end
