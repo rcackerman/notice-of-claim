1c03dd31e4a40a40893474f9858fe5d502bf7dda
Rebecca <rcackerman@gmail.com>
2016-01-19 22:46:13 -0500

    Moving boolean lookups to application helper so they're available throughout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def filter_trues object
+    booleans = get_booleans(object.class)
+    trues = booleans.select do |k,v|
+      object.send(k) == true
+    end
+
+    trues
+  end
+
+  def human_boolean label, boolean_value
+    render "shared/human_boolean", locals: {item_label: label, boolean_value: boolean_value}
+  end
+ 
+  private
+  def get_booleans model
+    booleans = Hash[model.columns.select do |c|
+      c.type == :boolean
+    end.map{ |c| [c.name.to_sym, c.human_name] }]
+  end
+
 end
--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -1,23 +1,2 @@
 module NoticesHelper
-
-  def filter_trues object
-    booleans = get_booleans(object.class)
-    trues = booleans.select do |k,v|
-      object.send(k) == true
-    end
-
-    trues
-  end
-
-  def human_boolean label, boolean_value
-    render "shared/human_boolean", locals: {item_label: label, boolean_value: boolean_value}
-  end
- 
-  private
-  def get_booleans model
-    booleans = Hash[model.columns.select do |c|
-      c.type == :boolean
-    end.map{ |c| [c.name.to_sym, c.human_name] }]
-  end
-
 end
