ff835ffcc64fa308782264ec687dc12c011c17c6
Rebecca <rcackerman@gmail.com>
2016-04-04 21:08:25 -0400

    Fixing capitalization for the list of officers

--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -19,21 +19,23 @@ module NoticesHelper
 
   def generate_notice_claims notice
     claims = Array.new
-    officers_list = generate_officers notice
+    officers_list = generate_officers(notice).map{
+                        |o| o.titleize
+                    }.to_sentence
     if notice.officer_arrested_no_probable_cause == true
-      claims << "Claimant was subjected to false arrest and false imprisonment by NYPD officers #{officers_list.to_sentence.titleize}."
+      claims << "Claimant was subjected to false arrest and false imprisonment by NYPD officers #{officers_list}."
     end
     if notice.officer_injured_me == true
-      claims << "Claimant suffered a battery and was subjected to excessive force #{generate_injury_details notice.physical_injury} at the hands of NYPD officers #{officers_list.to_sentence.titleize}."
+      claims << "Claimant suffered a battery and was subjected to excessive force #{generate_injury_details notice.physical_injury} at the hands of NYPD officers #{officers_list}."
     end
     if notice.officer_threatened_injury == true
-      claims << "Claimant was subjected to an assault by NYPD officers #{officers_list.to_sentence.titleize}."
+      claims << "Claimant was subjected to an assault by NYPD officers #{officers_list}."
     end
     if notice.officer_searched == true
-      claims << "Claimant was subjected to an illegal search of their property when NYPD officers #{officers_list.to_sentence.titleize} searched their #{generate_searched_objects notice.searched_object}."
+      claims << "Claimant was subjected to an illegal search of their property when NYPD officers #{officers_list} searched their #{generate_searched_objects notice.searched_object}."
     end
     if notice.officer_took_property == true || notice.officer_damaged_property == true || notice.officer_destroyed_property == true
-      claims << "Claimant’s property, to wit #{generate_lost_objects notice}, was #{pick_if_seized_damaged notice} by NYPD officers #{officers_list.to_sentence.titleize}. As a result, claimant was subjected to #{pick_property_claim_type notice}."
+      claims << "Claimant’s property, to wit #{generate_lost_objects notice}, was #{pick_if_seized_damaged notice} by NYPD officers #{officers_list}. As a result, claimant was subjected to #{pick_property_claim_type notice}."
     end
 
     claims.join " "
