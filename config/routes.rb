b4ea11364b36f8cf55c4c5eccdfe516df7700972
Rebecca <rcackerman@gmail.com>
2016-05-26 21:37:37 -0400

    Adding sign in and logout, as well as page to change passwords. Addresses issue #16

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations]                                          
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
+
   resources :outputs
 
   root 'screeners#new'
@@ -14,7 +19,6 @@ Rails.application.routes.draw do
   get 'about' => 'static#about', as: :about
   get 'contact' => 'static#contact', as: :contact
   get 'next_steps' => 'static#next_steps', as: :next_steps
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
