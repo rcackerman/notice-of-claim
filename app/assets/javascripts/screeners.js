e927873494e08803cae1bbc22e3f9a50db935940
Rebecca <rcackerman@gmail.com>
2016-05-28 10:57:05 -0400

    Fixing turbolinks issues

--- app/assets/javascripts/screeners.js
+++ app/assets/javascripts/screeners.js
@@ -1,10 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //
-$(function() {
+//
+var ready = function() {
   // datepickers
   $('#screener_incident_occurred_on').datepicker({
       container: $("#screener_incident_occurred_on").parent(),
       autoclose: true
   });
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
